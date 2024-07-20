e138e00a5432f027a37f19c7b46788a4d6b55e30
STEPiK0911 <136521792+STEPiK0911@users.noreply.github.com>
2024-07-20 23:37:29 +0300

    Delete RoutePlannerApi directory

--- RoutePlannerApi/Models/Point.cs
+++
@@ -1,9 +0,0 @@
-namespace RoutePlannerApi.Models
-{
-    public class Point
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Height { get; set; }
-    }
-}
